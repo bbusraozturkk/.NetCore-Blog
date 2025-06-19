ed92a6960d254c52d6da1111beea9e4ec01f60a6
bbusraozturkk <202003011037@dogus.edu.tr>
2025-06-19 16:32:52 +0300

    Areas Klasörü Eklendi

---
+++ Areas/Admin/Controllers/WriterController.cs
@@ -0,0 +1,39 @@
+﻿using CoreDemo.Areas.Admin.Models;
+using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WriterList()
+        {
+            var jsonWriters = JsonConvert.SerializeObject(writers);
+            return View(jsonWriters);
+        }
+
+        public IActionResult GetWriterByID(int writerid)
+        {
+            var findWriter = writers.FirstOrDefault(x => x.Id == writerid);
+            var jsonWriters = JsonConvert.SerializeObject(findWriter);
+            return Json(jsonWriters);
+        }
+
+        public static List<WriterClass> writers = new List<WriterClass>
+        {
+            new WriterClass
+            {
+                Id=1,
+                Name="Ayşe"
+            }
+        };
+    }
+}
---
+++ Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -0,0 +1,28 @@
+﻿using Business_Layer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic1 : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = bm.GetList().Count();
+            ViewBag.v2 = c.Contacts.Count();
+            ViewBag.v3 = c.Comments.Count();
+
+            string api = "44efc1590fc6cb8fad3eacb91afc291f";
+            string connection = "http://api.openweathermap.org/data/2.5/weather?q = istanbul & mode = xml & lang = tr & units = metric & appid = " + api;
+            XDocument document =XDocument.Load(connection);
+            ViewBag.v4=document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            return View();
+        }
+
+    }
+}
